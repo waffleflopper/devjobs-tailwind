85846953176ed517ee1f4888f0ea3403f9400dcc
Robert-AirM1 <robertbaddeley@gmail.com>
2023-03-31 16:58:04 -0500

    working on tablet/fullscreen top baf

---
+++ src/lib/components/Checkbox.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Style } from '../utils/tailwind';
+	import { svgs } from '$assets/icons';
+	export let checked: boolean = false;
+	export let className: string = '';
+
+	let styles = {
+		container: new Style('relative m-0 max-w-fit flex'),
+		label: new Style('cursor-pointer font-bold dark:text-white text-black'),
+		checkbox: new Style(''),
+		input: new Style({
+			base: 'peer appearance-none h-6 w-6 bg-slate-400 m-0 rounded-sm cursor-pointer',
+			focus: 'outline outline-indigo-800 outline-offset-1',
+			checked: 'bg-indigo-800'
+		})
+	};
+
+	console.log(styles.input);
+</script>
+
+<div class=" {styles.container}">
+	<label for="fullTime" class="{styles.label} "> Full-Time Only </label>
+</div>
--- src/lib/components/InputGroup.svelte
+++ src/lib/components/InputGroup.svelte
@@ -1,1 +1,33 @@
-<div class="sm:flex rounded-md shadow-sm" />
+<script lang="ts">
+	import Button from '$components/Button.svelte';
+	import Checkbox from './Checkbox.svelte';
+</script>
+
+<div
+	class="flex shadow-sm bg-white dark:bg-[--light-midnight] rounded-lg items-center justify-center gap-px"
+>
+	<input
+		type="text"
+		placeholder="Filter by title, company, experience..."
+		class="h-20 px-7 border-solid shadow-sm -mt-px -ml-px mr-px first:rounded-t-lg last:rounded-b-lg sm:first:rounded-l-lg sm:mt-0 sm:first:ml-0 sm:first:rounded-tr-none sm:last:rounded-bl-none sm:last:rounded-r-lg relative dark:bg-[--light-midnight] focus:outline-none larger"
+	/>
+	<div class="divider" />
+	<input
+		type="text"
+		placeholder="Filter by location..."
+		class="h-20 px-7 flex-1 shadow-sm -mt-px -ml-px first:rounded-t-lg last:rounded-b-lg sm:first:rounded-l-lg sm:mt-0 sm:first:ml-0 sm:first:rounded-tr-none sm:last:rounded-bl-none sm:last:rounded-r-lg relative dark:bg-[--light-midnight] focus:outline-none"
+	/>
+	<div class="divider" />
+
+	<Checkbox />
+	<Button theme="primary" variant="button" name="searchButton" className="mx-4">Search</Button>
+</div>
+
+<style lang="postcss">
+	.divider {
+		@apply w-px h-20 bg-[#6E8098] opacity-20;
+	}
+	.larger {
+		flex: 1.3;
+	}
+</style>
