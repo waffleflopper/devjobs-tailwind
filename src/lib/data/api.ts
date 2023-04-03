7c32e6bfced5033502815330b5ae7e759ed7e02a
Robert-MacMini <robertbaddeley@gmail.com>
2023-04-02 21:50:30 -0500

    making a little data json api

---
+++ src/lib/data/api.ts
@@ -0,0 +1,34 @@
+import data from '$lib/data/data.json';
+//why did i use toUpperCase()? Because I'm being lazy, that's why.
+export const getJob = (id: number) => {
+	return data.find((job) => job.id === id);
+};
+
+export const getJobsByLocation = (location: string) => {
+	location = location.toUpperCase();
+	return data.filter((job) => job.location.toUpperCase().includes(location));
+};
+
+export const getJobsByPosition = (position: string) => {
+	position = position.toUpperCase();
+	return data.filter((job) => job.position.toUpperCase().includes(position));
+};
+
+export const getJobsByAny = (any: string) => {
+	//loop through all the jobs and each job object and check if object values includes any string
+	any = any.toUpperCase();
+	return data.filter((job) => {
+		for (const [key, value] of Object.entries(job)) {
+			if (typeof value === 'string' && value.toUpperCase().includes(any)) {
+				return job;
+			}
+		}
+	});
+};
+
+export const getJobsByContract = (contract: string) => {
+	contract = contract.toUpperCase();
+	if (contract === 'Full Time' || contract === 'Part Time') {
+		return data.filter((job) => job.contract.toUpperCase().includes(contract));
+	}
+};
